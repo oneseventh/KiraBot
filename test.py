c9c36603919fca7285e95c15a338f7c7cfe1f051
zzxz46412 <roqkf4641@gmail.com>
2022-08-30 19:40:05 +0900

    1

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from tabnanny import check
 from turtle import color
 from unicodedata import category
 from pyreadline import Readline
-from discord.ext import commands
-import discord
+from nextcord.ext import commands
+import nextcord
 import re
 import asyncio
 import os.path
@@ -14,32 +14,33 @@ import datetime
 import time
 from datetime import timedelta
 
-intents = discord.Intents.default()
+intents = nextcord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = commands.Bot(command_prefix='?', intents=intents) 
 
 channel_id = 0 # 채널 id
-
+#봇이 시작되면 터미널에 활성화 되었다고 출력
 @client.event
 async def on_ready():
     print("활성화")
     print(f"봇 이름 : {format(client.user.name)}")
 '''
 @client.command()
-async def set_welcome(ctx, channel: discord.TextChannel):
+async def set_welcome(ctx, channel: nextcord.TextChannel):
     await ctx.send(f"{channel.name} 채널에 환영인사를 남깁니다.")
 '''
 
 #역할을 부여하는 명령어
 @client.command()
-async def set_role(ctx, role: discord.Role):
+async def set_role(ctx, role: nextcord.Role):
     await ctx.send(f"{role.name} 역할을 부여합니다.")
     await ctx.author.add_roles(role)
 #로그 저장할 채널을 설정하는 명령어
 @client.command()
-async def log_channel(ctx, channel: discord.TextChannel):
-    path = "C:\\Users\\Administrator\\Desktop\\discord_bot\\ScinceBot\\setting.txt"
+async def log_channel(ctx, channel: nextcord.TextChannel):
+    path = "C:\\Users\\Administrator\\Desktop\\src\\discord-bot\\ScienceBot\\setting.txt"
     global channel_id
     if os.path.isfile(path):
         with open(path) as r:
@@ -55,8 +56,8 @@ async def log_channel(ctx, channel: discord.TextChannel):
         f.close()
     if channel_id != 0:
         channel = client.get_channel(channel_id)
-        embed=discord.Embed(title=f"{channel.name} 채널에 로그를 저장합니다.", description=f"한번 선택한 채널은 바꿀 수 없습니다\n(변경기능 개발중!)", color=0x5947FF)
-        embed.add_field(name="ID", value=f"```diff\n+ {channel_id}```", inline=False)
+        embed=nextcord.Embed(title=f"{channel.name} 채널에 로그를 저장합니다.", description=f"한번 선택한 채널은 바꿀 수 없습니다\n(변경기능 개발중!)", color=0x5947FF)
+        embed.add_field(name="Selected channel ID", value=f"```diff\n+ {channel_id}```", inline=False)
         embed.set_footer(text=f"Request by {ctx.author} ・ Developed by 동건#3038", icon_url=ctx.author.display_avatar)
         await channel.send(embed=embed)
 
@@ -72,7 +73,7 @@ async def on_raw_message_delete(payload):
         return
     now = time.localtime()
     channel = client.get_channel(channel_id)
-    embed=discord.Embed(title="님의 메세지를 삭제하였습니다", description=f"메세지가 삭제된 채널 <#{payload.cached_message.channel.id}>", color=0x5947FF)
+    embed=nextcord.Embed(title="님의 메세지를 삭제하였습니다", description=f"메세지가 삭제된 채널 <#{payload.cached_message.channel.id}>", color=0x5947FF)
     embed.set_author(name=payload.cached_message.author, icon_url=payload.cached_message.author.display_avatar)
     embed.add_field(name="삭제된 메세지 내용", value=payload.cached_message.content, inline=False)
     embed.add_field(name="삭제된 시간", value="``%04d-%02d-%02d %02d:%02d:%02d``" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec), inline=False)
@@ -86,7 +87,7 @@ async def on_message_edit(before, after):
         return
     now = time.localtime()
     channel = client.get_channel(channel_id)
-    embed=discord.Embed(title="님의 메세지를 수정하였습니다", description=f"메세지가 수정된 채널 <#{before.channel.id}>", color=0xE189FC)
+    embed=nextcord.Embed(title="님의 메세지를 수정하였습니다", description=f"메세지가 수정된 채널 <#{before.channel.id}>", color=0xE189FC)
     embed.set_author(name=before.author, icon_url=before.author.display_avatar)
     embed.add_field(name="수정전 메세지 내용", value=before.content, inline=True)
     embed.add_field(name="수정후 메세지 내용", value=after.content, inline=True)
@@ -94,19 +95,23 @@ async def on_message_edit(before, after):
     embed.add_field(name="IDs", value=f"```diff\n+ {before.id} (message) \n+ {int(before.guild.id)} (guild) \n+ {int(channel_id)} (channel) ```", inline=False)
     embed.set_footer(text="Developed by 동건#3038")
     await channel.send(embed=embed)
+
+
 #역할이 추가되었을 때 로그를 저장하는 이벤트
 @client.event
 async def on_guild_role_create(role):
     channel = client.get_channel(channel_id)
-    embed=discord.Embed(title=f"<{role.name}> 역할이 생성되었습니다", color=0x00FF22)
+    embed=nextcord.Embed(title=f"<{role.name}> 역할이 생성되었습니다", color=0x00FF22)
     embed.add_field(name="IDs", value=f"```diff\n+ {role.id} (role) \n+ {int(role.guild.id)} (guild)```", inline=False)
     embed.set_footer(text="Developed by 동건#3038")
     await channel.send(embed=embed)
+
+
 #역할이 삭제되었을 때 로그가 저장되는 이벤트
 @client.event
 async def on_guild_role_delete(role):
     channel = client.get_channel(channel_id)
-    embed=discord.Embed(title=f"<{role.name}> 역할이 삭제되었습니다", color=0xFF0000)
+    embed=nextcord.Embed(title=f"<{role.name}> 역할이 삭제되었습니다", color=0xFF0000)
     embed.add_field(name="IDs", value=f"```diff\n+ {role.id} (role) \n+ {int(role.guild.id)} (guild)```", inline=False)
     embed.set_footer(text="Developed by 동건#3038")
     await channel.send(embed=embed)
@@ -115,7 +120,7 @@ async def on_guild_role_delete(role):
 async def on_guild_role_update(before, after):
     channel = client.get_channel(channel_id)
     checking = False
-    embed=discord.Embed(title=f"<{before.name}> 역할이 업데이트 되었습니다", color=after.colour)
+    embed=nextcord.Embed(title=f"<{before.name}> 역할이 업데이트 되었습니다", color=after.colour)
     if(before.name != after.name):
         embed.add_field(name=f"{before.name} -> {after.name}", inline=False)
         checking = True
@@ -140,25 +145,26 @@ async def on_guild_role_update(before, after):
 #채널이 추가되었을 때 로그가 저장되는 이벤트
 @client.event
 async def on_guild_channel_create(channel):
-    channel = client.get_channel(channel_id)
-    embed=discord.Embed(title=f"<{channel.name}> 채널이 생성되었습니다", color=0x00FF22)
+    log_channel = client.get_channel(channel_id)
+    embed=nextcord.Embed(title=f"<{channel}> 채널이 생성되었습니다", color=0x00FF22)
     embed.add_field(name="IDs", value=f"```diff\n+ {channel.id} (role) \n+ {int(channel.guild.id)} (guild)```", inline=False)
     embed.set_footer(text="Developed by 동건#3038")
-    await channel.send(channel.abc.GuildChannel.CategoryChannel.name)
+    await log_channel.send(embed=embed)
+
 #채널이 삭제되었을 때 로그가 저장되는 이벤트
 @client.event
 async def on_guild_channel_delete(channel):
-    channel = client.get_channel(channel_id)
-    embed=discord.Embed(title=f"<{channel.name}> 채널이 삭제되었습니다", color=0xFF0000)
+    log_channel = client.get_channel(channel_id)
+    embed=nextcord.Embed(title=f"<{channel}> 채널이 삭제되었습니다", color=0xFF0000)
     embed.add_field(name="IDs", value=f"```diff\n+ {channel.id} (role) \n+ {int(channel.guild.id)} (guild)```", inline=False)
     embed.set_footer(text="Developed by 동건#3038")
-    await channel.send(embed=embed)
+    await log_channel.send(embed=embed)
 #채널이 수정되었을 때 로그가 저장되는 이벤트
 @client.event
 async def on_guild_channel_update(before, after):
     channel = client.get_channel(channel_id)
     checking = False
-    embed=discord.Embed(title=f"<{before.name}> 채널이 업데이트 되었습니다", color=0xFFF700)
+    embed=nextcord.Embed(title=f"<{before.name}> 채널이 업데이트 되었습니다", color=0xFFF700)
     if(before.name != after.name):
         embed.add_field(name="이름",value=f"{before.name} -> {after.name}", inline=False)
         checking = True
@@ -188,7 +194,7 @@ async def on_guild_channel_update(before, after):
 @client.event
 async def on_invite_create(invite):
     channel = client.get_channel(channel_id)
-    embed=discord.Embed(title=f"<#{invite.channel.name}> 채널에서 초대코드가 생성되었습니다", color=0x86FFC3)
+    embed=nextcord.Embed(title=f"<#{invite.channel.name}> 채널에서 초대코드가 생성되었습니다", color=0x86FFC3)
     embed.add_field(name="생성자",value=f"``{invite.inviter}``", inline=True)
     embed.add_field(name="초대코드",value=f"{invite.code}", inline=True)
     embed.add_field(name="초대코드 만료기간",value=f"``{invite.expires_at}``\n(UTC+00:00 기준)", inline=True)
@@ -204,11 +210,53 @@ async def on_invite_create(invite):
 async def on_invite_delete(invite):
     channel = client.get_channel(channel_id)
     now = time.localtime()
-    embed=discord.Embed(title=f"<#{invite.channel.name}> 채널에서 초대코드가 삭제되었습니다", color=0xFF0000)
+    embed=nextcord.Embed(title=f"<#{invite.channel.name}> 채널에서 초대코드가 삭제되었습니다", color=0xFF0000)
     embed.add_field(name="초대코드",value=f"{invite.code}", inline=True)
     embed.add_field(name="초대코드가 삭제된 일시",value="``%04d-%02d-%02d %02d:%02d:%02d``" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec), inline=True)
     embed.set_footer(text="Developed by 동건#3038")
     await channel.send(embed=embed)
-    
 
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(channel_id)
+    embed=nextcord.Embed(title=f"님이 서버에 입장하셨습니다", color=0x00FF00)
+    embed.set_author(name=member, icon_url=member.display_avatar)
+    embed.add_field(name="서버에 입장한 시일",value=f"``{member.joined_at}``\n(UTC+00:00 기준)", inline=True)
+    embed.add_field(name="USER ID", value=f"```diff\n- {member.id}```", inline=False)
+    embed.set_footer(text="Developed by 동건#3038")
+    await channel.send(embed=embed)
+
+
+@client.event
+async def on_member_update(before, after):
+    channel = client.get_channel(channel_id)
+    embed=nextcord.Embed(title=f"{after.name}님의 서버 정보가 수정되었습니다", color=0x00FF00)
+    checkig = False
+    if(before.display_name != after.display_name):
+        embed.add_field(name="서버 닉네임", value=f"{before.display_name} -> {after.display_name}", inline=False)
+        checking = True
+    if checking == True:
+        embed.add_field(name="Guild ID", value=f"```diff\n+ {int(after.guild.id)}```", inline=False)
+        embed.set_footer(text="Developed by 동건#3038")
+        await channel.send(embed=embed)
+
+@client.event
+async def on_user_update(before, after):
+    channel = client.get_channel(channel_id)
+    embed=nextcord.Embed(title=f"{after.name}님의 프로필 정보가 수정되었습니다", color=0x00FF00)
+    checking = False
+    if before.name != after.name:
+        embed.add_field(name="프로필 닉네임", value=f"{before.name} -> {after.name}", inline=False)
+        checking = True
+    if before.discriminator != after.discriminator:
+        embed.add_field(name="프로필 디스택", value=f"{before.discriminator} -> {after.discriminator}", inline=False)
+        checking = True
+    if before.avatar != after.avatar:
+        embed.add_field(name="프로필 사진", value=f"``{before.display_avatar}`` -> ``{after.display_avatar}``", inline=False)
+        checking = True
+    if checking == True:
+        embed.add_field(name="User ID", value=f"```diff\n+ {int(after.id)} (user)```", inline=False)
+        embed.set_footer(text="Developed by 동건#3038")
+        await channel.send(embed=embed)
+    
 client.run(token=open(".token", "r").readline())
